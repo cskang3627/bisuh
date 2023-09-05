16384d6616d9806458310d168312ba0fd588bc84
cskang3627 <cskang3627@gmail.com>
2023-09-05 15:10:43 -0700

    intially delete impletment

--- bot.py
+++ bot.py
@@ -5,46 +5,20 @@ from dateparser import parse
 from datetime import datetime
 import json
 import asyncpg
+import asyncio
 
 # TODO consider .env instead of json
-# config stores prefix, token
 with open("config.json") as f:
     scrt = json.load(f)
 
 TOK = scrt["token"]
 PGPW = scrt["pgpw"]
 
-#POSTGRESQL
-'''
-
-
-cur.execute(""" CREATE TABLE IF NOT EXISTS events(
-            event_id SERIAL PRIMARY KEY,
-            event_name VARCHAR(255),
-            date DATE,
-            start_time TIME,
-            end_time TIME, 
-            server_id VARCHAR(255)
-            
-);
- """)
-
-cur.execute(""" INSERT INTO person (event_id, event_name, date, start_time, end_time, server_id) VALUES
-            (1, 'Chance', 26, 'm'),
-            (2, 'Wen', 22, 'f')
- """)
-
-conn.commit()
-cur.close()
-conn.close()
-'''
-
-# TO CHANGE: check intents
+# TODO: check intents
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-#bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 async def pg_pool():
@@ -55,7 +29,8 @@ async def pg_pool():
             start_date TIMESTAMP,
             end_date TIMESTAMP, 
             guild_id BIGINT,
-            creator_id BIGINT); """)
+            creator_id BIGINT,
+                              ); """)
 
 @bot.event
 async def on_ready():
@@ -88,38 +63,71 @@ async def on_guild_join(guild):
 '''
 
 
-'''
-TODO:
-1. check if created event is later than current time
-2. check if dup exists (name okay, time not okay)
-3. display as embeded prompt for readiblity
-4. emoji for rsvp
-5. pull data: 
-    user ID
-    server ID
-6. generate eventID (auto incrememt from sql)
 
-
-'''
-@bot.tree.command(name="create", description="create new event")
-async def create(interaction: discord.Interaction, name:str, when:str, duration:str = '0 hour', location:str = '' ):
+@bot.tree.command(name="create", description="Create new event. Timezone is set to UTC if not specified.")
+async def create(interaction: discord.Interaction, name:str, when:str, 
+                 duration:str = '0 hour', timezone:str = 'UTC', location:str = '', description:str = ''):
    
     #TODO: try catch for parse returning none
-    when_parsed = parse(when, settings = {'PREFER_DATES_FROM':'future'})
-    end_parsed = parse(duration, settings =  {'RELATIVE_BASE':when_parsed, 'PREFER_DATES_FROM':'future'})
+    try:
+        when_parsed = parse(when, settings = {'PREFER_DATES_FROM':'future', 'TIMEZONE' : timezone})
+        end_parsed = parse(duration, settings =  {'RELATIVE_BASE':when_parsed, 'PREFER_DATES_FROM':'future', 'TIMEZONE': timezone})
+    except:
+        await interaction.response.send_message(content = "Failed to parse when or duration.")
+        return
+
+    if when_parsed is None or end_parsed is None: 
+        await interaction.response.send_message(content = "Failed to parse when or duration.")
+    if len(name) > 255:
+        await interaction.response.send_message(content = "Name of the event is too long.")
+        return
+    
     when_unix = int(when_parsed.timestamp())
     end_unix = int(end_parsed.timestamp())
-    print(when_parsed)
-    print(end_parsed)
-    '''
-    date = parse(date, settings=setting).strftime('%Y-%m-%d')
-    start_time = parse(start_time, settings=setting).strftime('%H:%M')
-    end_time = parse(end_time, settings=setting).strftime('%H:%M')
-    '''
     await bot.pg_conn.execute(""" INSERT INTO events(event_name, start_date, end_date, guild_id, creator_id ) VALUES ($1,$2,$3,$4,$5)""",
                               name, when_parsed, end_parsed, interaction.guild_id, interaction.user.id)
     await interaction.response.send_message(content = f"Event {name}: <t:{when_unix}:F> ~ <t:{end_unix}:F> has been created!")
 
+# TODO: limit same name up to 5 events, dup delete with 5 number emoji
+@bot.tree.command(name="delete", description="Delete an existing event by its name.")
+async def delete(interaction: discord.Interaction, name: str):
+
+    records = await bot.pg_conn.fetch("SELECT event_id, start_date, end_date, creator_id FROM events WHERE event_name = $1 AND guild_id = $2",
+                                      name, interaction.guild_id)
+
+    if len(records) == 0:
+        await interaction.response.send_message(content="No events found.")
+        return
+
+    if not (interaction.user.id == records[0]['creator_id'] or interaction.user.guild_permissions.administrator):
+        await interaction.response.send_message(content="Only the creator and administrator can delete.")
+        return
+
+    if len(records) == 1:
+        await bot.pg_conn.execute("DELETE FROM events WHERE event_id = $1", records[0]['event_id'])
+        await interaction.response.send_message(content=f"Event {name} has been deleted.")
+        return
+
+    # If multiple events, let the user choose
+    options = []
+    for i, record in enumerate(records):
+        options.append(f"{i+1}: {name} on {record['start_date']} ~ {record['end_date']}")
+
+    options_str = "\n".join(options)
+    await interaction.response.send_message(content=f"Multiple events found with the name {name}. Please choose one to delete:\n{options_str}")
+
+    def check(m):
+        return m.author == interaction.user and m.content.isdigit() and 1 <= int(m.content) <= len(records)
+
+    try:
+        msg = await bot.wait_for('message', timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+        await interaction.followup.send(content='Time ran out. No events were deleted.')
+        return
+
+    index_to_delete = int(msg.content) - 1
+    await bot.pg_conn.execute("DELETE FROM events WHERE event_id = $1", records[index_to_delete]['event_id'])
+    await interaction.followup.send(content=f"Event {name} on {records[index_to_delete]['start_date']} ~ {records[index_to_delete]['end_date']} has been deleted.")
 
 """
 @bot.command()
